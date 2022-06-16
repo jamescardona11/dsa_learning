0a375c521922ef888675e7f9e5d13dfeb1ef3837
jamescardona11 <jjcardonao@gmail.com>
2022-06-15 19:24:08 -0500

    moon and inversions

--- hackerearth/codemonk/monk_and_inversions/MoonAndInversions.java
+++ hackerearth/codemonk/monk_and_inversions/MoonAndInversions.java
@@ -7,7 +7,7 @@ public class MoonAndInversions {
 
   public static BufferedReader getBufferedReader() throws Exception {
     BufferedReader br;
-    File file = new File("../../in.txt");
+    File file = new File("in.txt");
     boolean existsFile = file.exists();
 
     if (existsFile) {
@@ -27,15 +27,36 @@ public class MoonAndInversions {
     int t = Integer.parseInt(br.readLine());
 
     // ------------- start algorithm
+    StringBuilder sb = new StringBuilder();
     for (int i = 0; i < t; i++) {
-      StringBuilder sb = new StringBuilder();
-      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-      int n = Integer.parseInt(st.nextToken());
-      int k = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(br.readLine());
+      int[][] matrix = new int[m][m];
+
+      // read
+      for (int j = 0; j < m; j++) {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int k = 0; k < matrix.length; k++) {
+          matrix[j][k] = Integer.parseInt(st.nextToken());
+        }
+      }
+
+      // count
+      int count = 0;
+      for (int y = 0; y < m; y++) {
+        for (int r = 0; r < m; r++) {
+          for (int j = y; j < m; j++) {
+            for (int k = r; k < m; k++) {
+              if (matrix[y][r] > matrix[j][k]) {
+                count++;
+              }
+            }
+          }
+        }
+
+      }
+      System.out.println(count);
 
     }
-
-    System.out.println("hola james");
 
   }
 
