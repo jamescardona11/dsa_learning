db9122eaed2400de622d3e8ee204fd875cc84a94
James Cardona <65878592+jamescardona11@users.noreply.github.com>
2023-02-04 13:03:49 -0500

    start polish notation

---
+++ leetcode/150.evaluate-reverse-polish-notation.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=150 lang=javascript
+ *
+ * [150] Evaluate Reverse Polish Notation
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+  for(let i = 0; i< tokens.length; i++) {
+    tokens.pop
+  }
+    
+};
+// @lc code=end
+
