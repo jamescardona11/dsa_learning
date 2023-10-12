ecce404ba08672d4d0e597e23d184f81ad7dc8b1
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-10-11 21:52:35 -0500

    non adjacent sum solution

---
+++ _courses/fcc_dp/non_adjacent_sum.js
@@ -0,0 +1,20 @@
+function nonAdjacentSum(nums){
+  return nonAdjacentSumDS(nums, 0, {})
+}
+
+function nonAdjacentSumDS(nums, i, memo){
+  if(i >= nums.length) return 0
+  if(i in memo){
+    return memo[i]
+  }
+
+
+  memo[i] = Math.max(nums[i] + nonAdjacentSumDS(nums, i+2, memo), nonAdjacentSumDS(nums, i+1, memo))
+  return memo[i]
+
+
+}
+
+const nums = [2, 4, 5, 12, 7];
+const r = nonAdjacentSum(nums)
+console.log(r)
